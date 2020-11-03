9084481f136e1f7c8b0dc72c3a9dda5e4c8ba161
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2020-11-03 16:21:24 -0500

    Pair programming with Michelle, added test cases and fixed some bugs

--- unittests.py
+++ unittests.py
@@ -23,13 +23,14 @@ class TestSkiDay(unittest.TestCase):
     cls.mtn1.setGreenTrailObjects("Slide", "Green", True, "Bolton")
     cls.mtn1.setGreenTrailObjects("Snowball", "Green", True, "Bolton")
     cls.mtn1.setGreenTrailObjects("Upper Villager", "Green", True, "Bolton")
+
     cls.mtn2.setGreenTrailObjects("Cross Over", "Green", False, "Stowe")
     cls.mtn2.setGreenTrailObjects("Inspiration", "Green", False, "Stowe")
 
     cls.mtn3.setGreenTrailObjects("Deer Run", "Green", False, "Jay Peak")
     cls.mtn3.setGreenTrailObjects("Queen's Highway", "Green", True, "Jay Peak")
     cls.mtn3.setGreenTrailObjects("Kangaroo Trail", "Green", True, "Jay Peak")
-    
+
     # Blue trails
     cls.mtn1.setBlueTrailObjects("Sure Shot", "Blue", False, "Bolton")
     cls.mtn1.setBlueTrailObjects("Timberline Run", "Blue", True, "Bolton")
@@ -47,26 +48,28 @@ class TestSkiDay(unittest.TestCase):
 
 
     # Black Trails
-    cls.mtn1.setBlackTrailObjects("Cougar", "Black", False, "Bolton")
+    cls.mtn1.setBlackTrailObjects("Cougar", "Black", True, "Bolton")
     cls.mtn1.setBlackTrailObjects("Bolton Outlaw", "Black", False, "Bolton")
     cls.mtn1.setBlackTrailObjects("Upper Crossover", "Black", False, "Bolton")
     cls.mtn1.setBlackTrailObjects("SpellBinder", "Black", False, "Bolton")
-    cls.mtn1.setBlackTrailObjects("Upper Tattle Tale", "Black", False, "Bolton")
+    cls.mtn1.setBlackTrailObjects("Upper Tattle Tale", "Black", True, "Bolton")
     cls.mtn1.setBlackTrailObjects("Hard Luck", "Black", False, "Bolton")
+
     cls.mtn2.setBlackTrailObjects("Midway", "Black", False, "Stowe")
     cls.mtn2.setBlackTrailObjects("Hayride", "Black", False, "Stowe")
+
     cls.mtn3.setBlackTrailObjects("The Jet", "Black", False, "Jay Peak")
 
 
     # Double Black Trails
-    cls.mtn1.setDoubleBlackTrailObjects("Five Corners","Double Black", True, "Bolton")
+    cls.mtn1.setDoubleBlackTrailObjects("Five Corners", "Double Black", True, "Bolton")
     cls.mtn1.setDoubleBlackTrailObjects("Vermont 200", "Double Black", False, "Bolton")
-    cls.mtn2.setDoubleBlackTrailObjects("Spruce Line","Double Black",False,"Stowe")
+    cls.mtn2.setDoubleBlackTrailObjects("Spruce Line", "Double Black", True, "Stowe")
 
     cls.mtn3.setDoubleBlackTrailObjects("Cayonland", "Double Black", False, "Jay Peak")
     cls.mtn3.setDoubleBlackTrailObjects("Purgatory", "Double Black", True, "Jay Peak")
 
-    
+
     # Glade Trails
     cls.mtn1.setGladeTrailObjects("Vista Glades", "Glade", True, "Bolton")
     cls.mtn2.setGladeTrailObjects("Upper Nose Dive", "Glade", False, "Stowe")
@@ -76,9 +79,9 @@ class TestSkiDay(unittest.TestCase):
     # price, howFar
     cls.skier1 = skierSnowboarder.SkierSnowboarder("Michelle", 20, "Intermediate", 69, 60, True)
 
-    cls.skier2 = skierSnowboarder.SkierSnowboarder("Michelle", 20, "Advanced", 109, 140, True)
+    cls.skier2 = skierSnowboarder.SkierSnowboarder("Lauren", 20, "Advanced", 109, 140, True)
 
-    cls.skier3 = skierSnowboarder.SkierSnowboarder("Michelle", 25, "Beginner", 105, 121, False)
+    cls.skier3 = skierSnowboarder.SkierSnowboarder("Jason", 25, "Beginner", 105, 121, False)
 
     cls.skier4 = skierSnowboarder.SkierSnowboarder("Zach", 21, "Beginner", 95, 141, False)
 
@@ -87,17 +90,19 @@ class TestSkiDay(unittest.TestCase):
     cls.skiDay.addVermontMountains(cls.mtn1)
     cls.skiDay.addVermontMountains(cls.mtn2)
     cls.skiDay.addVermontMountains(cls.mtn3)
-    
+
     # add the skiers/snowboarders to the ski day
     cls.skiDay.addSkierSnowboarder(cls.skier1)
     cls.skiDay.addSkierSnowboarder(cls.skier2)
     cls.skiDay.addSkierSnowboarder(cls.skier3)
     cls.skiDay.addSkierSnowboarder(cls.skier4)
+    cls.skiDay.getSkierSnowboarderList()
 
 
   def test_skiday(self):
     print("test_skiday()")
     # test pickVermontMountain in SkiDay
+
     self.skiDay.pickVermontMountain()
     mountains = self.skiDay.getFinalizedSkiDay()
     print(mountains)
@@ -106,10 +111,114 @@ class TestSkiDay(unittest.TestCase):
     # Lauren - Bolton, Stowe, Jay Peak
     # Jason - Bolton, Stowe, Jay Peak
     # Zach - Bolton and Jay Peak
-    mountainsSelected = {"Michelle": ["Bolton"], "Lauren": ["Bolton", "Stowe", "Jay Peak"],
-                         "Jason": ["Bolton", "Stowe", "Jay Peak"], "Zach": ["Bolton", "Jay Peak"]}
+    mountainsSelected = {"Michelle": {"VT Mountains": ["Bolton"]}, "Lauren": {"VT Mountains": ["Bolton", "Stowe", "Jay Peak"]},
+                         "Jason": {"VT Mountains": ["Bolton", "Stowe", "Jay Peak"]}, "Zach": {"VT Mountains": ["Bolton", "Jay Peak"]}}
+    self.assertEqual(mountainsSelected, mountains)
+
+
+
+    trailSelected = {'Michelle': {'VT Mountains': ['Bolton'], 'GreenTrails': [], 'BlueTrails': ['Timberline Run', 'Showtime'], 'BlackTrails': ['Cougar', 'Upper Tattle Tale'], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Lauren': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak'], 'GreenTrails': [], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': ['Five Corners', 'Spruce Line', 'Purgatory'],
+                    'GladeTrails': ['Vista Glades']}, 'Jason': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Cross Over', 'Inspiration', 'Deer Run', "Queen's Highway", 'Kangaroo Trail'], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Zach': {'VT Mountains': ['Bolton', 'Jay Peak'],
+                    'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Deer Run', "Queen's Highway", 'Kangaroo Trail'], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}}
+    # test determineTrailsForDay
+    self.skiDay.determineTrailsForDay()
+    trails = self.skiDay.getFinalizedSkiDay()
+    print("\n Trails \n")
+    self.assertEqual(trailSelected, trails)
+    print(trails)
+
+    correctSkiers = ["Michelle","Lauren","Jason","Zach"]
+    skierList = self.skiDay.getSkierSnowboarderList()
+    listReturned = []
+    for object in skierList:
+      listReturned.append(object.getName())
+    self.assertEqual(correctSkiers,listReturned)
+
+    correctMountains = ["Bolton","Stowe","Jay Peak"]
+    mtnList = self.skiDay.getVTMountainsList()
+    mtnReturned = []
+    for vt in mtnList:
+      mtnReturned.append(vt.getName())
+    self.assertEqual(correctMountains,mtnReturned)
+
+    skier5 = skierSnowboarder.SkierSnowboarder("Jackson", 24, "Beginner", 89, 138, False)
+    self.skiDay.addSkierSnowboarder(skier5)
+    correctSkiers = ["Michelle","Lauren","Jason","Zach","Jackson"]
+    skierList = self.skiDay.getSkierSnowboarderList()
+    listReturned = []
+    for object in skierList:
+      listReturned.append(object.getName())
+    self.assertEqual(correctSkiers,listReturned)
+
+    self.skiDay.pickVermontMountain()
+    mountains = self.skiDay.getFinalizedSkiDay()
+    print(mountains)
+
+    addedSkierMountains = {'Michelle': {'VT Mountains': ['Bolton']}, 'Lauren': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak']},
+                        'Jason': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak']}, 'Zach': {'VT Mountains': ['Bolton', 'Jay Peak']},
+                        'Jackson': {'VT Mountains': ['Bolton', 'Jay Peak']}}
+    self.assertEqual(addedSkierMountains, mountains)
+
+    addedSkierNewTrails = {'Michelle': {'VT Mountains': ['Bolton'], 'GreenTrails': [], 'BlueTrails': ['Timberline Run', 'Showtime'], 'BlackTrails':
+      ['Cougar', 'Upper Tattle Tale'], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Lauren': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak'], 'GreenTrails':
+      [], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': ['Five Corners', 'Spruce Line', 'Purgatory'], 'GladeTrails': ['Vista Glades']}, 'Jason':
+      {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Cross Over',
+       'Inspiration', 'Deer Run', "Queen's Highway", 'Kangaroo Trail'], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []},
+       'Zach': {'VT Mountains': ['Bolton', 'Jay Peak'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Deer Run',
+      "Queen's Highway", 'Kangaroo Trail'], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Jackson': {'VT Mountains': ['Bolton', 'Jay Peak'],
+       'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Deer Run', "Queen's Highway", 'Kangaroo Trail'], 'BlueTrails': [],
+     'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}}
+
+    self.skiDay.determineTrailsForDay()
+    trails = self.skiDay.getFinalizedSkiDay()
+    self.assertEqual(addedSkierNewTrails, trails)
+
+    mtn4 = vtmountain.vtmountain("Smugglers Notch", 3, 4, 1, 2, 1, 11, 110, True, 90)
+    correctMountains = ["Bolton","Stowe","Jay Peak","Smugglers Notch"]
+    self.skiDay.addVermontMountains(mtn4)
+    mtnList = self.skiDay.getVTMountainsList()
+    mtnReturned = []
+    for vt in mtnList:
+      mtnReturned.append(vt.getName())
+    self.assertEqual(correctMountains,mtnReturned)
+
+    self.skiDay.pickVermontMountain()
+    mountains = self.skiDay.getFinalizedSkiDay()
+    newMountainList = {'Michelle': {'VT Mountains': ['Bolton']},
+                       'Lauren': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak', 'Smugglers Notch']},
+                       'Jason': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak', 'Smugglers Notch']},
+                       'Zach': {'VT Mountains': ['Bolton', 'Jay Peak', 'Smugglers Notch']},
+                       'Jackson': {'VT Mountains': ['Bolton', 'Jay Peak']}}
+    self.assertEqual(newMountainList, mountains)
+
+    mtn4.setGreenTrailObjects("New Trail 1", "Green", True, "Smugglers Notch")
+    mtn4.setBlueTrailObjects("New Trail 2", "Blue", True, "Smugglers Notch")
+    mtn4.setBlackTrailObjects("New Trail 3", "Black", True, "Smugglers Notch")
+    mtn4.setDoubleBlackTrailObjects("New Trail 4", "Double Black", True, "Smugglers Notch")
+    mtn4.setGladeTrailObjects("New Trail 5", "Glade", True, "Smugglers Notch")
+
+    self.skiDay.determineTrailsForDay()
+    trails = self.skiDay.getFinalizedSkiDay()
+    newAddedTrails = {'Michelle': {'VT Mountains': ['Bolton'], 'GreenTrails': [], 'BlueTrails': ['Timberline Run', 'Showtime'],
+                                   'BlackTrails': ['Cougar', 'Upper Tattle Tale'], 'DoubleBlackTrails': [], 'GladeTrails': []},
+                      'Lauren': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak', 'Smugglers Notch'], 'GreenTrails': [], 'BlueTrails': [],
+                                 'BlackTrails': [], 'DoubleBlackTrails': ['Five Corners', 'Spruce Line', 'Purgatory', 'New Trail 4'],
+                                 'GladeTrails': ['Vista Glades', 'New Trail 5']}, 'Jason': {'VT Mountains': ['Bolton', 'Stowe', 'Jay Peak',
+                                  'Smugglers Notch'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager',
+                                'Cross Over', 'Inspiration', 'Deer Run', "Queen's Highway", 'Kangaroo Trail', 'New Trail 1'], 'BlueTrails': [],
+                                 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Zach': {'VT Mountains': ['Bolton', 'Jay Peak',
+                                'Smugglers Notch'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Deer Run', "Queen's Highway",
+                          'Kangaroo Trail', 'New Trail 1'], 'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}, 'Jackson': {'VT Mountains':
+                       ['Bolton', 'Jay Peak'], 'GreenTrails': ['Round Robin', 'Magic Carpet', 'Slide', 'Snowball', 'Upper Villager', 'Deer Run', "Queen's Highway", 'Kangaroo Trail'],
+                       'BlueTrails': [], 'BlackTrails': [], 'DoubleBlackTrails': [], 'GladeTrails': []}}
+
+    self.assertEqual(trails,newAddedTrails)
+
+    #self.assertEqual(addedSkierNewTrails, trails)
+    # Must uncomment this to fail, and comment this to pass
+    #wrongTrails = self.skiDay.wrongPickVermontMountain()
+    #self.assertEqual(wrongTrails, mountains)
 
 
 if __name__ == "__main__":
-  print("YAY")
   unittest.main()
--- vtmountain.py
+++ vtmountain.py
@@ -1,41 +1,56 @@
 import trail
 
-class vtmountain: 
+class vtmountain:
 
-    def __init__(self, name, greenTrails, blueTrails, blackTrails, doubleBlackTrails, gladeTrials, totalTrails, distanceFromUVM, nightSkiing,dayPassPrice):
-      self.greenTrailObjects = []
-      self.blueTrailObjects = []
-      self.blackTrailObjects = []
-      self.doubleBlackTrailObjects = []
-      self.gladeTrailObjects = []
-      self.name = name
-      self.greenTrails = greenTrails
-      self.blackTrails = blackTrails
-      self.blueTrails = blueTrails
-      self.doubleBlackTrails = doubleBlackTrails
-      self.totalTrails = totalTrails
-      self.distanceFromUVM = distanceFromUVM
-      self.nightSkiing = nightSkiing
-      self.gladeTrials = gladeTrials
-      self.dayPassPrice = dayPassPrice
+    def __init__(self, name, greenTrails, blueTrails, blackTrails, doubleBlackTrails, gladeTrails, totalTrails, distanceFromUVM, nightSkiing, dayPassPrice):
+        self.greenTrailObjects = []
+        self.blueTrailObjects = []
+        self.blackTrailObjects = []
+        self.doubleBlackTrailObjects = []
+        self.gladeTrailObjects = []
+        self.name = name
+        self.greenTrails = greenTrails
+        self.blackTrails = blackTrails
+        self.blueTrails = blueTrails
+        self.doubleBlackTrails = doubleBlackTrails
+        self.totalTrails = totalTrails
+        self.distanceFromUVM = distanceFromUVM
+        self.nightSkiing = nightSkiing
+        self.gladeTrails = gladeTrails
+        self.dayPassPrice = dayPassPrice
 
     def getName(self):
-      return self.name
+        return self.name
 
     def getAllGreenTrails(self):
-      return self.greenTrails
+        return self.greenTrails
 
     def getAllBlueTrails(self):
-      return self.blueTrails
+        return self.blueTrails
 
     def getAllBlackTrails(self):
-      return self.blackTrails
+        return self.blackTrails
 
     def getAllDoubleBlackTrails(self):
-      return self.doubleBlackTrails
+        return self.doubleBlackTrails
 
     def getAllGladeTrails(self):
-      return self.gladeTrials
+        return self.gladeTrails
+
+    def getAllGreenTrailObjects(self):
+        return self.greenTrailObjects
+
+    def getAllBlueTrailObjects(self):
+        return self.blueTrailObjects
+
+    def getAllBlackTrailObjects(self):
+        return self.blackTrailObjects
+
+    def getAllDoubleBlackTrailObjects(self):
+        return self.doubleBlackTrailObjects
+
+    def getAllGladeTrailObjects(self):
+        return self.gladeTrailObjects
 
     def getTotalAmount(self):
         if self.dayPassPrice < 0:
@@ -43,7 +58,7 @@ class vtmountain:
         return self.dayPassPrice
 
     def nightSkiing(self):
-      return self.nightSkiing
+        return self.nightSkiing
 
     def getHowFarFromUVM(self):
       if self.distanceFromUVM < 0:
